4271df72cbb912b2465c8e3440ea13cda7cb7736
FabrazJVargas <m.vargas.or10@gmail.com>
2024-11-14 13:45:42 -0500

    Actualizacion

---
+++ src/app/modal/editar-user/editar-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-user.component.html',
+  styleUrl: './editar-user.component.scss'
+})
+export class EditarUserComponent {
+
+}
